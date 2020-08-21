dc0588ba89c6fb5c2ead7be8fde0cb61c9d5972b
Darya Khodyreva <dashah1990@mail.ru>
2020-08-21 08:59:03 +0500

    task 1.3 solved

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,31 @@
+'use strict';
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let input = [percent, contribution, amount, date];
+    for (let i = 0; i < input.length - 1; i++) {
+        if (!isNaN(input[i])) {
+            input[i] = Number(input[i]);
+            if (!isNaN(input[i])) {
+                console.log(`Параметр содержит неправильное значение ${input[i]}`);
+        }
+        } 
+    }
+    let loanBody = amount - contribution;
+    let month = (date.getFullYear() - new Date().getFullYear()) * 12 + date.getMonth() - new Date().getMonth();
+    if ((date.getDate() - new Date().getDate()) > 0) {
+        month ++;
+    }
+    percent = percent / 100 * 1 / 12;
+    let pageseMujore = loanBody * (percent + percent / (((1 + percent) ** month) - 1));
+    let totalAmount = pageseMujore * month;
+    totalAmount = totalAmount.toFixed(2);
+    console.log(parseFloat(totalAmount));
+    parseFloat(totalAmount);
+    return parseFloat(totalAmount);
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    if (!name) {
+        name = "Аноним"
+    }
+    return `Привет, мир! Меня зовут ${name}`;
 }
